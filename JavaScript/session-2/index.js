bdfd1395d1429d6d793d00881b3d9d9177649e3b
jessica <jvictoriaw@hotmail.co.uk>
2023-05-10 13:07:32 +0100

    task 2 - adding string values

--- JavaScript/session-2/index.js
+++ JavaScript/session-2/index.js
@@ -1,4 +1,5 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
 console.log('Hello everyone!');
+console.log('Hey ' + 'how are you?');
 
