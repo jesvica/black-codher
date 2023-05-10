2d604e1ac0888764831d817737cff97ca8dbb816
jessica <jvictoriaw@hotmail.co.uk>
2023-05-10 18:58:17 +0100

    task 3

--- JavaScript/session-2/index.js
+++ JavaScript/session-2/index.js
@@ -2,4 +2,7 @@
 // You can see where this file is connected to your HTML in the index.html on line 14
 console.log('Hello everyone!');
 console.log('Hey ' + 'how are you?');
-
+console.log(document.querySelector('#description').innerHTML)
+console.log(document.querySelectorAll('.about').innerHTML)
+console.log(document.querySelectorAll('.about').innerHTML)
+console.log(document.querySelectorAll('.about'))
