00b6df8538e2f980220662e45453545c35ad6ddd
jessica <jvictoriaw@hotmail.co.uk>
2023-05-17 14:27:36 +0100

    task 1 - random number while loop

---
+++ JavaScript/session-6/index.js
@@ -0,0 +1,28 @@
+// Use this randomNumber inside of the while/for loops to generate a random number
+// const randomNumber = Math.round(Math.random() * 10);
+
+// ****************************************************TASK 1****************************************************
+
+let counter = 5;
+
+while (counter <= 10) {
+    const randomNumber = Math.round(Math.random() * 10);
+    counter++;
+    console.log(randomNumber);
+}
+
+
+
+// ****************************************************TASK 2****************************************************
+
+
+
+
+
+
+
+
+
+
+
+
