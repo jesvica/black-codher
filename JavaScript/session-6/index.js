3dbc2426fa88e96871d6e9862d2be9bdeaea1b08
jessica <jvictoriaw@hotmail.co.uk>
2023-05-17 15:54:16 +0100

    task 2 - for loop

--- JavaScript/session-6/index.js
+++ JavaScript/session-6/index.js
@@ -3,20 +3,23 @@
 
 // ****************************************************TASK 1****************************************************
 
-let counter = 5;
+// let counter = 5;
 
-while (counter <= 10) {
-    const randomNumber = Math.round(Math.random() * 10);
-    counter++;
-    console.log(randomNumber);
-}
+// while (counter <= 10) {
+//     const randomNumber = Math.round(Math.random() * 10);
+//     counter++;
+//     console.log(randomNumber);
+// }
 
 
 
 // ****************************************************TASK 2****************************************************
+let counter = 1;
 
-
-
+for (let counter = 1; counter <= 10; counter++) {
+    const randomNumber = Math.round(Math.random() * 10);
+    console.log(randomNumber);
+}
 
 
 
