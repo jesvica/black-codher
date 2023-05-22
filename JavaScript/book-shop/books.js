28695dbf05e9dab4ac12278bcc4acd6901a0e4ad
jessica <jvictoriaw@hotmail.co.uk>
2023-05-22 14:56:24 +0100

    beginning of book shop website

---
+++ JavaScript/book-shop/books.js
@@ -0,0 +1,41 @@
+export const books = [
+    {
+        name: 'book1',
+        author: 'author',
+        price: '£10',
+        description: 'description',
+        image: URL('')
+    },
+
+    {
+        name: 'book2',
+        author: 'author',
+        price: '£10',
+        description: 'description',
+        image: URL('')
+    },
+
+    {
+        name: 'book3',
+        author: 'author',
+        price: '£10',
+        description: 'description',
+        image: URL('')
+    },
+
+    {
+        name: 'book4',
+        author: 'author',
+        price: '£10',
+        description: 'description',
+        image: URL('')
+    },
+
+    {
+        name: 'book5',
+        author: 'author',
+        price: '£10',
+        description: 'description',
+        image: URL('')
+    }
+]
---
+++ JavaScript/book-shop/main.js
@@ -0,0 +1,5 @@
+import { books } from "/books.js;"
+
+const bookShop = document.querySelector('.bookShop')
+const heading = document.createElement('h1');
+heading.textContent = "Book Shop"
